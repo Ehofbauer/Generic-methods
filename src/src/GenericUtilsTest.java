9cf80618e082c6ea657c91dcfc805774b91d4da3
Ehofbauer <ehofbauer@student.tgm.ac.at>
2025-02-23 22:30:06 +0100

    test class completed

--- src/src/GenericUtilsTest.java
+++ src/src/GenericUtilsTest.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class GenericUtilsTest {
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        GenericUtils.reverse(intArray);
+        System.out.println("Reversed Integer Array: " + Arrays.toString(intArray));
+
+        String[] strArray = {"A", "B", "C", "D"};
+        GenericUtils.reverse(strArray);
+        System.out.println("Reversed String Array: " + Arrays.toString(strArray));
+
+        Double[] doubleArray = {1.5, 2.5, 3.5};
+        System.out.println("Sum of Double Array: " + GenericUtils.sum(doubleArray));
+
+        Integer[] numArray = {10, 20, 30};
+        System.out.println("Sum of Integer Array: " + GenericUtils.sum(numArray));
+    }
 }
